ce7e4b935733dadb24d178fcbecac4dac3809de0
Breno-Jansen <brenojaccioly@gmail.com>
2025-04-25 21:51:20 -0300

    Update projeto.py

--- projeto.py
+++ projeto.py
@@ -8,87 +8,120 @@ def menu_inicial():
     if opcao_inicial == '1':
         cadastrar()
     elif opcao_inicial == '2':
-        ()
+        efetuar_login()
 
 
 def cadastrar():
-    email = cadastro_usuario()
-    senha = cadastro_senha()
+    os.system('cls')
+    email_cd = cadastro_usuario()
+    senha_cd = cadastro_senha()
 
-    cadastro_usuario()
-    cadastro_senha()
     with open('bancodedados.txt', 'a', encoding = 'utf-8') as arquivo:
-        arquivo.write(f'{email}, {senha}')
+        arquivo.write(f'{email_cd}, {senha_cd}\n')
     menu_principal()
+
+def efetuar_login():
+    os.system('cls')
+    # Login de usuario:
+    usuario = login_usuario()
+    # Achando a linha do usuario no banco de dados
+    with open('bancodedados.txt', 'r') as arquivo:
+        lines = arquivo.readlines()
+    for line in lines:
+        if line.find(usuario) != -1:
+            break            
+            
+    # Senha:
+    while True:
+        print('Login: Sua senha tem 8 caractéries')
+        senha_log = input('Senha: ').strip()
+        
+        # Se a senha for a mesma da linha do usuario no banco de dados
+        if senha_log in line and len(senha_log) == 8:
+            os.system('cls')
+            print('LOGIN EFETIVADO')
+            menu_principal()
+            return senha_log
+            
+        else:
+            os.system('cls')
+            print('Senha incorreta')
 
     
 def cadastro_usuario():
-    os.system('clear')
     print ('Cadastro : digite o usuário (e-mail)')
     print ('O usuário precisa terminar com @gmail.com ou @ufrpe.br')
     while True:
-        email = input ('Usuário: ').strip()
+        email_cd = input ('Usuário: ').strip()
         # Restrição de e-mails para o usuário
-        if ('@ufrpe.br' in email) or ('@gmail.com' in email):
-            os.system('clear')
+        if ('@ufrpe.br' in email_cd) or ('@gmail.com' in email_cd):
+            os.system('cls')
             print('Usuário valido')
-            return email
-
-        ###elif '@gmail.com' in usuario_in:
-            os.system('clear')
-            print('Usuário valido')
-            return usuario_in
+            return email_cd
         else:
-            os.system('clear')
+            os.system('cls')
             print('Usuário inválido. E-mails aceitos: @ufrpe.br ou @gmail.com')
-            cadastro_usuario()
 
 def cadastro_senha():
     while True:
         print('Sua senha precisa ter 8 caractéries')
-        senha = input('Senha: ').strip()
+        senha_cd = input('Senha: ').strip()
         
         # Restrições da senha
-        if len(senha) != 8:
-            os.system('clear')
+        if len(senha_cd) != 8:
+            os.system('cls')
             print('senha inválida. Sua senha precisa ter 8 caractéries')
-            cadastro_usuario()
         else:
-            os.system('clear')
+            os.system('cls')
             print('LOGIN EFETIVADO')
-            return senha
+            return senha_cd
+
+
+def login_usuario():
+    while True:
+        print('Login: digite seu e-mail:')
+        email_log = input('Usuário: ')
+
+        with open('bancodedados.txt', 'r') as arquivo:
+            txt = arquivo.read()
+
+        if email_log in txt:
+            print('Usuário valido')
+            return email_log
+        else:
+            os.system('cls')
+            print('Usuário inválido. Esse e-mail não está cadastrado')
+    
+        
+
 
 
 
 def menu_principal():
-    print(f'Bem vindo!')
+    print(f'🇧‌ 🇪‌ 🇲‌  🇻‌ 🇮‌ 🇳‌ 🇩‌ 🇴‌  🇦‌ 🇴‌  🇧‌ 🇦‌ 🇿‌ 🇦‌ 🇷‌')
     # Exibir opções da página
-    print ('1. Jogar Quizz \n2. Jogar mini-game \n3. Pontuação \n4. Configurações \n0. Sair ')
+    print ('1. Acessar produtos a venda  \n2. Adicionar produto \n3. Configurações \n0. Sair ')
     resposta = input ('Digite o número para continuar ou sair: ')
     if resposta == '1':
-        os.system('clear')
-        print('Quizz')
+        os.system('cls')
+        print('Produtos disponíveis')
     elif resposta == '2':
-        os.system('clear')
-        print('Mini-game')
+        os.system('cls')
+        print('Adicionar produto')
     elif resposta == '3':
-        os.system('clear')
-        print('Pontuação')
-    elif resposta == '4':
-        os.system('clear')
-        print('Configurações')
+        os.system('cls')
+        print('Configurações da conta')
     else:
-        os.system('clear')
+        os.system('cls')
         print("Encerrando Programa\nLimpando a tela em:")
         for i in range(3, 0, -1):
             print(f"{i}...")
             time.sleep(1)
-        os.system('clear')
-
+        os.system('cls')
 
 
 def main():
-    os.system('clear')
+    os.system('cls')
     menu_inicial()
 
 if __name__ == '__main__':
