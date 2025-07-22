aae0a92a4b89114aaeb92dccfecbc1f863580888
Breno-Jansen <brenojaccioly@gmail.com>
2025-07-21 23:17:37 -0300

    Update CamelCase nas funções menu.py

--- menu.py
+++ menu.py
@@ -10,7 +10,7 @@ class Menu:
     def __init__(self, usuario):
         self.usuario = usuario
         
-    def menu_inicial(self):
+    def MenuInicial(self):
         '''
             A primeira tela do programa é esse menu que contem as opções de Cadastro e Login para acessar o bazar.
             Essa função é chamada pela main() para sempre começar por aqui.
@@ -19,103 +19,103 @@ class Menu:
 
         '''
         while True:
-            self.limpar_terminal()
+            self.LimparTerminal()
             # Exibir opções no Menu
             width = console.size.width
             painel_width = max(40, min(80, width - 10)) # Tamanho do painel
 
             texto_menu = (
             '[bold cyan]Para entrar no Bazar escolha uma opção:[/bold cyan]\n\n'
-            '[bold white]1 - Cadastro[/bold white]\n'
-            '[bold white]2 - Login[/bold white]'
+            '1 - Cadastro\n'
+            '2 - Login'
             )
 
-            painel = Panel(texto_menu, title= '[bold white]MENU INICIAL[/bold white]', width=painel_width, border_style='purple')
+            painel = Panel(texto_menu, title= '[bold magenta]MENU INICIAL[/bold magenta]', width=painel_width)
             console.print(painel)
 
-            opcao_inicial = Prompt.ask('[bold white]Digite a opção desejada[/bold white]').strip()
+            opcao_inicial = input('Digite a opção desejada: ').strip()
             if opcao_inicial == '1':
-                self.menu_cadastro()
+                self.MenuCadastro()
                 break
             elif opcao_inicial == '2':
-                self.menu_login()
+                self.MenuLogin()
                 break
             else:
                 console.print('[red]Escolha 1 ou 2[/red]')
                 input('Pressione Enter para tentar novamente...')
-    def menu_cadastro(self):
+    def MenuCadastro(self):
         '''
             Similar ao menu inicial, aqui também são exibidas opções para acessar o Bazar: novo usuário ou voltar
             Recebe as entradas 1, 2 e X como opções de cadastrar, voltar para fazer o login e voltar ao menu inicial
             Em caso de uma entrada inválida continua até receber 1, 2 ou X.
         '''
         while True:
-            self.limpar_terminal()
+            self.LimparTerminal()
             # Opções do cadastro, ir para login e voltar
             width = console.size.width
             painel_width = max(40, min(80, width - 10))
 
             texto_menu = (
             '[bold cyan]Para realizar o cadastro:[/bold cyan]\n\n'
-            '[bold white]1 - Novo usuário[/bold white]\n'
-            '[bold white]2 - Já tem conta? Faça o login[/bold white]\n'
-            '[bold white]X - Voltar ao menu inicial[/bold white]'
+            '1 - Novo usuário\n'
+            '2 - Já tem conta? Faça o login\n'
+            'X - Voltar ao menu inicial'
             )
 
-            painel = Panel(texto_menu, title="[bold white]CADASTRO[/bold white]", width=painel_width, border_style='purple')
+            painel = Panel(texto_menu, title="[bold magenta]CADASTRO[/bold magenta]", width=painel_width)
             console.print(painel)
 
-            opcao_menu_cadastro = Prompt.ask('[bold white]Sua escolha[/bold white]').strip().lower()
+            opcao_menu_cadastro = input('Sua escolha: ').strip().lower()
             if opcao_menu_cadastro == '1':
-                self.usuario.cadastrar()
+                self.usuario.Cadastrar()
                 break
             elif opcao_menu_cadastro == '2':
-                self.menu_inicial()
+                self.MenuInicial()
                 break
             elif opcao_menu_cadastro == 'x':
-                self.menu_inicial()
+                self.MenuInicial()
                 break 
             else:
                 console.print('[red]Opção inválida[/red]')
                 input('Pressione Enter para tentar novamente...')
 
-    def menu_login(self):
+    def MenuLogin(self):
         '''
             Nesse menu são exibidas opções para acessar o Bazar ou para voltar. 
             Recebe as entradas 1, 2 e X como opções de login, esqueci a senha e voltar ao menu inicial.
             Em caso de uma entrada inválida continua até receber 1, 2 ou X.
         '''
         while True:
-            self.limpar_terminal()
+            self.LimparTerminal()
             # Opções do login, esqueci senha e voltar
             width = console.size.width
             painel_width = max(40, min(80, width - 10))
 
             texto_menu = (
             '[bold cyan]Para realizar o login:[/bold cyan]\n\n'
-            '[bold white]1 - Usuário e senha[/bold white]\n'
-            '[bold white]2 - Esqueci a senha[/bold white]\n'
-            '[bold white]X - Voltar ao menu inicial[/bold white]'
+            '1 - Usuário e senha\n'
+            '2 - Esqueci a senha\n'
+            'X - Voltar ao menu inicial'
             )
 
-            painel = Panel(texto_menu, title="[bold white]LOGIN[/bold white]", width=painel_width, border_style='purple')
+            painel = Panel(texto_menu, title="[bold magenta]LOGIN[/bold magenta]", width=painel_width)
             console.print(painel)
-            opcao_menu_login = Prompt.ask('[bold white]Sua escolha[/bold white]').strip().lower()
+            opcao_menu_login = input('Sua escolha: ').strip().lower()
             if opcao_menu_login == '1':
-                self.usuario.efetuar_login()
+                self.usuario.EfetuarLogin()
                 break
             elif opcao_menu_login == '2':
-                self.usuario.esqueci_senha()
+                self.usuario.EsqueciSenha()
                 break
             elif opcao_menu_login == 'x':
-                self.menu_inicial()
+                self.MenuInicial()
                 break 
             else:
                 console.print('[red]Opção inválida[/red]')
                 input('Pressione Enter para tentar novamente...')
 
     
-    def menu_principal(self, usuario):
+    def MenuPrincipal(self, usuario):
         '''
             Este é o principal local de acesso as funcionalidades do Bazar Brejó.
             Tem 4 opções de entrada para acessar itens à venda, lançar itens, acessar configurações e sair.
@@ -130,50 +130,50 @@ class Menu:
         # Exibir opções da página
        
         while True:
-            self.limpar_terminal()
+            self.LimparTerminal()
             width = console.size.width
             painel_width = max(50, min(80, width - 10)) 
 
             titulo = '[bold magenta]BAZAR BREJÓ[/bold magenta]'   
             texto = (
                 '[bold cyan]BEM-VINDO AO BAZAR BREJÓ[/bold cyan]\n\n'
-                '[bold white]1 - Acessar itens à venda[/bold white]\n'
-                '[bold white]2 - Lançar item[/bold white]\n'
-                '[bold white]3 - Configurações[/bold white]\n'
-                '[bold white]X - Sair[/bold white]'
+                '1 - Acessar itens à venda\n'
+                '2 - Lançar item\n'
+                '3 - Configurações\n'
+                'X - Sair'
             )
-            painel = Panel(texto, title = titulo, width = painel_width, border_style='purple')
+            painel = Panel(texto, title = titulo, width = painel_width)
             console.print(painel)
-            resposta_mp = Prompt.ask('[bold white]Digite a opção desejada: [/bold white]')
+            resposta_mp = Prompt.ask('[bold yellow]Digite a opção desejada: [/bold yellow]')
             if resposta_mp == '1':
-                self.limpar_terminal()
+                self.LimparTerminal()
                 console.print('[bold green]Itens disponíveis[/bold green]')
-                Item.comprar_itens(usuario)
+                Item.ComprarItens(usuario)
                 break
             elif resposta_mp == '2':
-                self.limpar_terminal()
+                self.LimparTerminal()
                 console.print('[green]Adicionar item[/green]')
-                Item.lancar_item(usuario)
+                Item.LancarItem(usuario)
                 break
             elif resposta_mp == '3':
-                self.limpar_terminal()
-                self.menu_config(usuario)
+                self.LimparTerminal()
+                self.MenuConfig(usuario)
                 break
             elif resposta_mp == 'x':
                 # Animação da saída do terminal
-                self.limpar_terminal()
+                self.LimparTerminal()
                 print('Encerrando Programa\nLimpando a tela em:')
                 for i in range(3, 0, -1):
                     print(f"{i}...")
                     time.sleep(1)
-                self.limpar_terminal()
+                self.LimparTerminal()
                 break
             else:
                 console.print('[red]Opção inválida[/red]')
                 input('Pressione Enter para tentar novamente...')
-                self.limpar_terminal()
+                self.LimparTerminal()
 
-    def menu_config(self, usuario):
+    def MenuConfig(self, usuario):
         '''
             Aqui são exibidas as opções das configurações como feedback, mudar nome, mudar senha, excluir conta e voltar.
 
@@ -185,56 +185,56 @@ class Menu:
         from sistema import menu_global
 
         while True:
-            self.limpar_terminal()
+            self.LimparTerminal()
             width = console.size.width
             painel_width = max(50, min(80, width - 10))
 
-            titulo = '[bold white]CONFIGURAÇÕES[/bold white]'
+            titulo = '[bold magenta]CONFIGURAÇÕES[/bold magenta]'
             texto = (
-                '[bold white]1 - Feedback[/bold white]\n'
-                '[bold white]2 - Mudar nome[/bold white]\n'
-                '[bold white]3 - Mudar senha[/bold white]\n'
-                '[bold white]4 - Excluir conta[/bold white]\n'
-                '[bold white]5 - Ver extrato[/bold white]\n'
-                '[bold white]X - Voltar[/bold white]'
+                '1 - Feedback\n'
+                '2 - Mudar nome\n'
+                '3 - Mudar senha\n'
+                '4 - Excluir conta\n'
+                '5 - Ver extrato\n'
+                'X - Voltar'
             )
-            painel = Panel(texto, title = titulo, width = painel_width, border_style='purple')
+            painel = Panel(texto, title = titulo, width = painel_width)
             console.print(painel)
-            resposta_mc = Prompt.ask('[bold white]Digite a opção desejada: [/bold white]').strip().lower()
+            resposta_mc = Prompt.ask('[bold yellow]Digite a opção desejada: [/bold yellow]').strip().lower()
 
             if resposta_mc == '1':
-                self.limpar_terminal()
+                self.LimparTerminal()
                 console.print('[bold green]Feedback[/bold green]')
-                self.usuario.feedback(usuario)
+                self.usuario.Feedback(usuario)
                 break
             elif resposta_mc == '2':
-                self.limpar_terminal()
+                self.LimparTerminal()
                 console.print('[bold green]Mudar nome da conta[/bold green]')
-                self.usuario.mudar_nome_config(usuario)
+                self.usuario.MudarNomeConfig(usuario)
                 break
             elif resposta_mc == '3':
-                self.limpar_terminal()
+                self.LimparTerminal()
                 console.print('[bold green]Mudar senha da conta[/bold green]')
-                self.usuario.mudar_senha_config(usuario)
+                self.usuario.MudarSenhaConfig(usuario)
                 break
             elif resposta_mc == '4':
-                self.limpar_terminal()
-                self.usuario.excluir_conta(usuario)
+                self.LimparTerminal()
+                self.usuario.ExcluirConta(usuario)
                 break
             elif resposta_mc == '5':
-                self.limpar_terminal()
-                self.usuario.mostrar_extrato(usuario)
+                self.LimparTerminal()
+                self.usuario.MostrarExtrato(usuario)
             elif resposta_mc == 'x':
-                self.limpar_terminal()
-                menu_global.menu_principal(usuario)
+                self.LimparTerminal()
+                menu_global.MenuPrincipal(usuario)
                 break
             else:
                 console.print('[red]Opção inválida[/red]')
                 input('Pressione Enter para tentar novament...')
-                self.limpar_terminal()
+                self.LimparTerminal()
     
     @staticmethod
-    def limpar_terminal():
+    def LimparTerminal():
         '''
             Aqui está a função mais usada do código.
             Ela limpa o terminal tanto em sistemas Windows quanto Mac e Linux
